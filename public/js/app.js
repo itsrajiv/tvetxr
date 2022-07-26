f9e4a8508c53905572c422549153f116a69d9547
zulzayn <zulwaqarzain96@gmail.com>
2022-07-27 07:40:24 +0800

    update

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\zulwa\AppData\Local\Programs\laragon\tvetxr\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\zulwa\AppData\Local\Programs\laragon\tvetxr\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\laragon\www\tvetxr\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\tvetxr\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -219,6 +219,22 @@
                                         <input class="block mt-1 w-full form-input rounded-md shadow-sm"   type="password" name="password_confirmation" required autocomplete="new-password">
                                     </div>
                         
+                                    <br>
+           
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <x-jet-label value="{{ __('Role') }}" />
+                                                <select name="role" id="role" class="form-control custom-select" data-placeholder="Choose Class Owner" style="box-shadow: 0 .125rem .25rem rgba(0,0,0,.075)!important;" tabindex="1">
+                                                    <option value="">-- Choose Role --</option>
+                                                    <option value="student">Student</option>
+                                                    <option value="lecturer">Lecturer</option>
+                                                </select>
+                                           
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
                                     <div class="flex items-center justify-end mt-4">
                                         <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
                                             Already registered?
